2083c2be0d2bd7068ecbba75bb461b690cced0b6
Ladislas GABRIEL <ladislas.gabriel.partner@decathlon.com>
2025-09-22 17:08:48 +0200

    🚀setup internationalization

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import {useTranslations} from "next-intl";
 
 export default function Home() {
+    const t = useTranslations('HomePage');
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -12,6 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
+          <h1>{t('title')}</h1>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
